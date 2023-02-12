3db1dfc24649af76135ef96012586f098410ad5f
b-tice <120678973+b-tice@users.noreply.github.com>
2023-02-12 11:24:15 -0800

    added comments and cleaned up code
    removed the unused effects code, commented and cleaned up the code.

--- Tremolo.cpp
+++ Tremolo.cpp
@@ -1,7 +1,36 @@
-// Brian Tice
+// Project: Tremolo-Bandpass Project
 //
-// Tremolo + Bandpass Project
+// Written by: Brian Tice, 2/12/2023
 //
+// Description: This file contains protoype code for a Tremolo-Bandpass 
+//              multi effect on the Daisy platform. The idea is to apply 
+//              a tremolo effect to only a selected frequency band, notated
+//              with both high and low frequency parameters available
+//              to the musican via potentiometers. 
+//
+//              This prototype is based on the example code provided by ElectroSmith: 
+//              https://github.com/electro-smith/DaisyExamples/tree/master/pod/MultiEffect
+//              While the effects aren't utilized, this code has functions for the rotary encoder, 
+//              potentiometers and LED's that are useful.
+//              
+//              This protoype apploes the built in Tremolo function available in daisysp
+//
+//              It also evaluates two types of bandpass filters:
+//
+//              1) State Variable Filter - based on the svf provided in daisysp
+//
+//              2) Bandpass derived from second order all pass - based on Fred Harris design,
+//                 code and details explained by Tom Erbe here:   
+//                 http://synthnotes.ucsd.edu/wp4/index.php/2019/11/09/second-order-allpass-filter/
+
+//              To Do: 
+//                      1) Build out circuit to include 5 potentiometers:
+//                          TremoloRate, Tremolo Depth, LowFreq, HighFreq, Wet/Dry.
+//                      2) Fine tune parameters for filters and tremolo. 
+//                      3) Write a library function for the Bandpass Derived from a
+//                         a Second Order All Pass and send to Electro Smith so they 
+//                         can include it in the open source libraries 
+//                      4) Documentation of parameters 
 
 #include "daisysp.h"            // Open Source DSP Library
 #include "daisy_pod.h"          // libDaisy, hardware abstraction library for
@@ -9,14 +38,19 @@
 
 #define PI 3.141592653589793
 
-// Set max delay time to 0.75 of samplerate.
-#define MAX_DELAY static_cast<size_t>(48000 * 2.5f)
-#define REV 5
-#define DEL 4
-#define TRM 2   // Tremolo Test State
-#define CRU 3
-#define BNP 1   // Band Pass SVF filter Test State
-#define SAB 0   // Second Order All Pass Bandpass Filter
+#define TRM 2   // Tremolo Test State. This is just a tremolo with depth and rate
+                // LED's are pink in this state. POT1 is rate, POT2 is depth.
+                // The parameters persist in other states, so you can dial in the
+                // tremolo here and it will persist in the SAB state
+
+#define BNP 1   // Band Pass SVF filter Test State. LED's are GREEN in this state.
+                // A test of the built in SVF.
+                // POT1 is center frequency, POT2 is q.
+
+#define SAB 0   // Second Order All Pass Bandpass Filter + Tremolo Test State
+                // This is our prototype. LED's are BLUE in this state.
+                // POT1 is center frequency, POT2 is q. Tremolo parameters 
+                // get tuned in the TRM state.
 
 using namespace daisysp;
 using namespace daisy;
@@ -25,28 +59,20 @@ static DaisyPod pod;
 
 static Tremolo                                   trem;
 static Svf                                       filt;
-
-static ReverbSc                                  rev;
-static DelayLine<float, MAX_DELAY> DSY_SDRAM_BSS dell;
-static DelayLine<float, MAX_DELAY> DSY_SDRAM_BSS delr;
 static Tone                                      tone;
-static Parameter deltime, cutoffParam, crushrate;
-int              mode = REV;
+static Parameter                                 deltime, cutoffParam, crushrate;
+int                                              mode = SAB;
 
-float currentDelay, feedback, delayTarget, cutoff;
-
-int   crushmod, crushcount;
-float crushsl, crushsr, drywet;
-
-// for Soap Filter
-float soap_cutoff = 400.0;
-float bw = 50.0;
-float in_0 = 0.0;           // input            x0
-float din_1 = 0.0;          // delayed input    x1
-float din_2 = 0.0;          // delayed input    x2
-float dout_1 = 0.0;         // delayed output   y1
-float dout_2 = 0.0;         // delayed output   y2
-float all_output = 0.0;     // all pass output  y0
+// for Bandpass derived from Second Order All Pass Filter
+// note, x_n and y_n's are from difference eqn.
+float soap_cutoff =     400.0;
+float bw =              50.0;
+float in_0 =            0.0;          // input            x0
+float din_1 =           0.0;          // delayed input    x1
+float din_2 =           0.0;          // delayed input    x2
+float dout_1 =          0.0;          // delayed output   y1
+float dout_2 =          0.0;          // delayed output   y2
+float all_output =      0.0;          // all pass output  y0
 float d;
 float tf;           // tangent bandwidth   
 float c;            // coefficient     
@@ -56,17 +82,11 @@ float sr;
 //Helper functions
 void Controls();
 
-void GetReverbSample(float &outl, float &outr, float inl, float inr);
-
-void GetDelaySample(float &outl, float &outr, float inl, float inr);
-
-void GetCrushSample(float &outl, float &outr, float inl, float inr);
-
 void GetTremoloSample(float &outl, float &outr, float inl, float inr);
 
 void GetBandPassSample(float &outl, float &outr, float inl, float inr);
 
-void GetSoapSample(float &outl, float &outr, float inl, float inr);
+void GetTremoloSoapSample(float &outl, float &outr, float inl, float inr);
 
 void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
                    AudioHandle::InterleavingOutputBuffer out,
@@ -84,12 +104,10 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
 
         switch(mode)
         {
-            case REV: GetReverbSample(outl, outr, inl, inr); break;
-            case DEL: GetDelaySample(outl, outr, inl, inr); break;
-            case CRU: GetCrushSample(outl, outr, inl, inr); break;
+
             case TRM: GetTremoloSample(outl, outr, inl, inr); break;
             case BNP: GetBandPassSample(outl, outr, inl, inr); break;
-            case SAB: GetSoapSample(outl, outr, inl, inr); break;
+            case SAB: GetTremoloSoapSample(outl, outr, inl, inr); break;
                       
             default: outl = outr = 0;
         }
@@ -112,41 +130,22 @@ int main(void)
     pod.SetAudioBlockSize(4);
     sample_rate = pod.AudioSampleRate();
     sr = sample_rate;
-    rev.Init(sample_rate);
     trem.Init(sample_rate);
     filt.Init(sample_rate);
-    dell.Init();
-    delr.Init();
     tone.Init(sample_rate);
 
     //set parameters
-    deltime.Init(pod.knob1, sample_rate * .05, MAX_DELAY, deltime.LOGARITHMIC);
     cutoffParam.Init(pod.knob1, 500, 20000, cutoffParam.LOGARITHMIC);
     crushrate.Init(pod.knob2, 1, 50, crushrate.LOGARITHMIC);
-
-    //reverb parameters
-    rev.SetLpFreq(18000.0f);
-    rev.SetFeedback(0.85f);
 
     //tremolo parameters
     trem.SetFreq(2);
     trem.SetDepth(0.75);
 
-    //bandpass filter settings
+    // SVF bandpass filter settings
     filt.SetFreq(300.0);
     filt.SetRes(0.85);
     filt.SetDrive(0.8);
-
-    //Soap filter settings
-    //d = -cos(2.0 * PI * (soap_cutoff/sample_rate));
-    //tf = tan(PI * (bw/sample_rate));      // tangent bandwidth   
-    //c = (tf - 1.0)/(tf + 1.0);            // coefficient     
-
-
-    //delay parameters
-    currentDelay = delayTarget = sample_rate * 0.75f;
-    dell.SetDelay(currentDelay);
-    delr.SetDelay(currentDelay);
 
     // start callback
     pod.StartAdc();
@@ -162,18 +161,7 @@ void UpdateKnobs(float &k1, float &k2)
 
     switch(mode)
     {
-        case REV:
-            drywet = k1;
-            rev.SetFeedback(k2);
-            break;
-        case DEL:
-            delayTarget = deltime.Process();
-            feedback    = k2;
-            break;
-        case CRU:
-            cutoff = cutoffParam.Process();
-            tone.SetFreq(cutoff);
-            crushmod = (int)crushrate.Process();
+
         case TRM:
             trem.SetFreq(k1*3);
             trem.SetDepth(k2);
@@ -204,29 +192,18 @@ void UpdateLeds(float k1, float k2)
 void Controls()
 {
     float k1, k2;
-    delayTarget = feedback = drywet = 0;
-
+   
     pod.ProcessAnalogControls();
     pod.ProcessDigitalControls();
 
     UpdateKnobs(k1, k2);
-
     UpdateEncoder();
-
     UpdateLeds(k1, k2);
-}
-
-void GetReverbSample(float &outl, float &outr, float inl, float inr)
-{
-    rev.Process(inl, inr, &outl, &outr);
-    outl = drywet * outl + (1 - drywet) * inl;
-    outr = drywet * outr + (1 - drywet) * inr;
 }
 
 void GetTremoloSample(float &outl, float &outr, float inl, float inr)
 {
     outl = trem.Process(inl);
-    //outr = trem.Process(inr);
     outr = outl;
 }
 
@@ -235,13 +212,11 @@ void GetBandPassSample(float &outl, float &outr, float inl, float inr)
 
     filt.Process(inl);
     outl = filt.Band();
-    //filt.Process(outl);
-    //outr = filt.Band();
     outr = outl;
 
 }
 
-void GetSoapSample(float &outl, float &outr, float inl, float inr) 
+void GetTremoloSoapSample(float &outl, float &outr, float inl, float inr) 
 {
 
     /*  float in_0 = 0.0;       // input            x0
@@ -257,8 +232,7 @@ void GetSoapSample(float &outl, float &outr, float inl, float inr)
     float c = (tf - 1.0)/(tf + 1.0);                  // coefficient     
 
     float orig_ileft = inl;
-    float orig_iright = inr;
-
+    
     // *******
     // Add in the tremolo
     inl = trem.Process(inl);
@@ -267,7 +241,7 @@ void GetSoapSample(float &outl, float &outr, float inl, float inr)
     // *******
 
     in_0 = inl;   
-    //outl = -c*in_0 + (d - d*c)*din_1 + din_2 - (d - d*c)*dout_1 + c*dout_2;
+    
     all_output = -c*in_0 + (d - d*c)*din_1 + din_2 - (d - d*c)*dout_1 + c*dout_2;
 
     // move samples in delay for next sample
@@ -284,32 +258,6 @@ void GetSoapSample(float &outl, float &outr, float inl, float inr)
 }
 
 
-void GetDelaySample(float &outl, float &outr, float inl, float inr)
-{
-    fonepole(currentDelay, delayTarget, .00007f);
-    delr.SetDelay(currentDelay);
-    dell.SetDelay(currentDelay);
-    outl = dell.Read();
-    outr = delr.Read();
 
-    dell.Write((feedback * outl) + inl);
-    outl = (feedback * outl) + ((1.0f - feedback) * inl);
-
-    delr.Write((feedback * outr) + inr);
-    outr = (feedback * outr) + ((1.0f - feedback) * inr);
-}
-
-void GetCrushSample(float &outl, float &outr, float inl, float inr)
-{
-    crushcount++;
-    crushcount %= crushmod;
-    if(crushcount == 0)
-    {
-        crushsr = inr;
-        crushsl = inl;
-    }
-    outl = tone.Process(crushsl);
-    outr = tone.Process(crushsr);
-} 
 
 
